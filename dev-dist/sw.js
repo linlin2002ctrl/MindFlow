cf5a26b18ea1a4f62364430651ee53021c957ab5
linlin2002ctrl <htetaungl451@gmail.com>
2025-08-24 20:53:48 +0630

    [dyad] Installed `framer-motion` to resolve import error. - wrote 2 file(s), added framer-motion package(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lhi33ue4j4"
+    "revision": "0.p300ttsfh08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
