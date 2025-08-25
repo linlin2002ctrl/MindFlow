397d3db3e61533160c1646f69ae81d85fad9e1fc
linlin2002ctrl <htetaungl451@gmail.com>
2025-08-25 20:08:09 +0630

    [dyad] Applied UI/UX typography guidelines to global CSS - wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3nl5edq9f3o"
+    "revision": "0.vm5i5ej5htg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
